dd2201253705abc20be1f15c9feabb209c955b5a
lazyBoy1909 <93031924+lazyBoy1909@users.noreply.github.com>
2022-06-06 16:33:07 +0700

    add UIImage extension to flip a image horizontally and vertically, add assets to project

---
+++ Crop_Image/ItemCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ItemCollectionViewCell.swift
+//  Crop_Image
+//
+//  Created by đào sơn on 06/06/2022.
+//
+
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Crop_Image/PHAssetExtension.swift
@@ -0,0 +1,8 @@
+//
+//  PHAssetExtension.swift
+//  Crop_Image
+//
+//  Created by đào sơn on 06/06/2022.
+//
+
+import Foundation
