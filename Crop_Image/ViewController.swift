b32d21b688984c2d0a0633dda42bc7adef6c5819
lazyBoy1909 <93031924+lazyBoy1909@users.noreply.github.com>
2022-06-06 14:51:24 +0700

    Initial Commit

---
+++ Crop_Image/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Crop_Image
+//
+//  Created by đào sơn on 06/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
