408c5c053494e4ffa0fb8de01d839451b6ea911f
lazyBoy1909 <93031924+lazyBoy1909@users.noreply.github.com>
2022-06-06 16:40:33 +0700

    create func get random image from library, and fetch PHAsset to UIImage with selected mode

--- Crop_Image/PHAssetExtension.swift
+++ Crop_Image/PHAssetExtension.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+import Photos
+import UIKit
+extension PHAsset
+{
+    static func loadRandomPHAssetImageFromGallery() ->PHAsset?
+    {
+        var assetImages: [PHAsset] = []
+        let assets = PHAsset.fetchAssets(with: PHAssetMediaType.image, options: nil)
+        assets.enumerateObjects{
+        (object,_,_) in
+            assetImages.append(object)
+        }
+        assetImages.reverse()
+        return assetImages.randomElement()
+    }
+    
+    func fetchImage(widthSize: Double, heightSize: Double, contentMode: PHImageContentMode) ->UIImage
+    {
+        let manager = PHImageManager.default()
+        var resultImage: UIImage!
+        let option = PHImageRequestOptions()
+        option.isSynchronous = true
+        manager.requestImage(for:  self, targetSize: CGSize(width: widthSize,height: heightSize), contentMode: contentMode, options: option, resultHandler: { (result, info)-> Void in
+            if let image = result
+            {
+                resultImage = image
+            }
+            else
+            {
+                print("Failed to get image from asset")
+                resultImage = UIImage(systemName: "exclamationmark.icloud.fill")!;
+            }
+        })
+        return resultImage
+    }
+}
