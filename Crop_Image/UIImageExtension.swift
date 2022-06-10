be30598cd7bb124d6fd51ae46f849044225f69c9
lazyBoy1909 <93031924+lazyBoy1909@users.noreply.github.com>
2022-06-10 09:02:41 +0700

    add func crop image and save image to photo library

--- Crop_Image/UIImageExtension.swift
+++ Crop_Image/UIImageExtension.swift
@@ -8,28 +8,40 @@
 import Foundation
 import UIKit
 extension UIImage {
-  
-  enum Axis {
-    case horizontal, vertical
-  }
-  
-  func flipped(_ axis: Axis) -> UIImage {
-    let renderer = UIGraphicsImageRenderer(size: size)
-    
-    return renderer.image {
-      let context = $0.cgContext
-      context.translateBy(x: size.width / 2, y: size.height / 2)
-      
-      switch axis {
-      case .horizontal:
-        context.scaleBy(x: -1, y: 1)
-      case .vertical:
-        context.scaleBy(x: 1, y: -1)
-      }
-      
-      context.translateBy(x: -size.width / 2, y: -size.height / 2)
-      
-      draw(at: .zero)
+    enum Axis {
+      case horizontal, vertical
     }
-  }
+  
+    func flipped(_ axis: Axis) -> UIImage {
+      let renderer = UIGraphicsImageRenderer(size: size)
+
+      return renderer.image {
+        let context = $0.cgContext
+        context.translateBy(x: size.width / 2, y: size.height / 2)
+      
+        switch axis {
+        case .horizontal:
+          context.scaleBy(x: -1, y: 1)
+        case .vertical:
+          context.scaleBy(x: 1, y: -1)
+        }
+      
+        context.translateBy(x: -size.width / 2, y: -size.height / 2)
+      
+        draw(at: .zero)
+        }
+    }
+    
+    //crop image to a Rect
+    static func cropImage(image: UIImage, toRect: CGRect) -> UIImage? {
+        let cgImage :CGImage! = image.cgImage
+        let croppedCGImage: CGImage! = cgImage.cropping(to: toRect)
+        return UIImage(cgImage: croppedCGImage)
+    }
+    
+    //save image to photo library
+    static func saveImage(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, nil, nil)
+    }
 }
+
---
+++ Crop_Image/UIImageViewExtension.swift
@@ -0,0 +1,8 @@
+//
+//  UIImageViewExtension.swift
+//  Crop_Image
+//
+//  Created by đào sơn on 10/06/2022.
+//
+
+import Foundation
