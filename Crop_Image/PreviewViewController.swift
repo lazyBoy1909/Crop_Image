03e3433a8ee103919cd83c4123fb21cf9c389e1e
lazyBoy1909 <93031924+lazyBoy1909@users.noreply.github.com>
2022-06-10 09:08:15 +0700

    create preview viewcontroller with save option

--- Crop_Image/PreviewViewController.swift
+++ Crop_Image/PreviewViewController.swift
@@ -9,21 +9,32 @@ import UIKit
 
 class PreviewViewController: UIViewController {
 
+    @IBOutlet private weak var previewImageView: UIImageView!
+    var previewImage: UIImage!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.title = "Preview"
+        addPreviewImage()
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func addPreviewImage()
+    {
+        self.previewImageView.image = previewImage
     }
-    */
 
+    @IBAction func saveButtonDidTap(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Save Image", message: "Do you want to save this image to your device?", preferredStyle: .actionSheet)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: {
+            _ in
+            UIImage.saveImage(image: self.previewImage)
+            let successAlert = UIAlertController(title: "Save Image", message: "Your image was saved successfully in your photo library!", preferredStyle: .alert)
+            let confirmAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+            successAlert.addAction(confirmAction)
+            self.present(successAlert, animated: true, completion: nil)
+        })
+        let cancelAction = UIAlertAction(title: "Cancle", style: .cancel, handler: nil)
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        self.present(alert, animated: true, completion: nil)
+    }
 }
